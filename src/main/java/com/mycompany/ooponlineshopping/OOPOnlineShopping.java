91bb38ca16801600c3523e5c3faa113467dfa966
Khyran Zarsuela <khyranzarsuela@gmail.com>
2026-06-05 20:10:46 +0800

    header pushed to main

--- src/main/java/com/mycompany/ooponlineshopping/OOPOnlineShopping.java
+++ src/main/java/com/mycompany/ooponlineshopping/OOPOnlineShopping.java
@@ -11,6 +11,7 @@ package com.mycompany.ooponlineshopping;
 public class OOPOnlineShopping {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Header h = new Header();
+       h.setVisible(true);
     }
 }
